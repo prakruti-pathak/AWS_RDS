98a6839499c3a3634e8def3f847712c945d90d7e
prakruti-pathak <prakrutipathak109@gmail.com>
2024-12-11 11:15:50 +0530

    added gitignore file

--- CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
+++ CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdcdc25b0943b7de43d31601a48adda5f6fdc705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc861b6be4e262cf69f02787438d7fc1aba5447")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
